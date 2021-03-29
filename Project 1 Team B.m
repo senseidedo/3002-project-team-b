75211876b0c0f43f870748391ee5341dd1fcec52
senseidedo <senseidedo@gmail.com>
2021-03-29 11:24:38 -0400

    fourier and newton
    defined some equations

--- Project 1 Team B.m
+++ Project 1 Team B.m
@@ -11,5 +11,9 @@ h = 200;
 w = 0.16;
 
 %code to solve
+%fourier
+qf = k*Af*(ta-tb)/sidef; %this shit is completely wrong
+%newton
+qh = h*Ah*(tc-td)/sideh; %this shit is also completely wrong but bear with me
 
 %code to plot
