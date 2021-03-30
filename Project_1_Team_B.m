e94908535b71c7c076c1debc3d2179c5aafffe8f
senseidedo <senseidedo@gmail.com>
2021-03-30 10:48:12 -0400

    Update Project_1_Team_B.m

--- Project_1_Team_B.m
+++ Project_1_Team_B.m
@@ -3,18 +3,15 @@ clearvars;
 
 %Equations from the problem
 
-a=
-b=
+a=[
+b=[
 
 %Create Augmented Matrix
-
 A=[a,b];
-
 [n,~]=size(A);
-
 k=1;
 
-% Creating Triangular Matrix
+%Creating Triangular Matrix
 for i=1:n-1
     for j=i+1:n
         m=A(j,i)/A(i,i);
@@ -28,9 +25,8 @@ x=zeros(n,1);
 x(end)=A(end,end)/A(end,end-1); %Generalized
 
 %Generalized
- for i=n-1:-1:1
+for i=n-1:-1:1
     x(i)=(A(i,end)-A(i,i+1:n)*x(i+1:n))/A(i,i);
- end
- x
- 
- %Code for plotting
+end
+x
+
