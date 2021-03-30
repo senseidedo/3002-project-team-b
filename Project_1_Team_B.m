8d075a014550471085563af5b5e22757f6f58988
senseidedo <senseidedo@gmail.com>
2021-03-29 21:46:44 -0400

    Gauss Elimination
    Gauss Elimination fixed

---
+++ Project_1_Team_B.m
@@ -0,0 +1,41 @@
+clc;
+clearvars;
+
+%Equations from the problem
+
+a=
+b=
+
+%Create Augmented Matrix
+
+A=[a,b];
+
+[n,~]=size(A);
+
+k=1;
+
+% Creating Triangular Matrix
+for i=1:n-1
+    for j=i+1:n
+        m=A(j,i)/A(i,i);
+        A(j,:)=A(j,:)-m*A(i,:);
+    end
+end
+
+
+%Back substitution
+x=zeros(n,1);
+
+%x(3)=A(3,4)/A(3,3);
+
+x(end)=A(end,end)/A(end,end-1); %Generalized
+
+
+% x(2)=(A(2,4)-A(2,3)*x(3))/A(2,2);
+% x(1)=(A(1,4)-A(1,3)*x(3)-A(1,2)*x(2))/A(1,1);
+
+%Generalized
+ for i=n-1:-1:1
+    x(i)=(A(i,end)-A(i,i+1:n)*x(i+1:n))/A(i,i);
+ end
+ x
